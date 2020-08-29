f5bbd16e9496a545227abf0b1cfae4ed5a776c49
cbertinci <jcbp.jcbp@gmail.com>
2020-08-29 00:59:33 -0300

    improve: add some bootstrap customization examples

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Boilerplate</h1>
-        <button className="btn btn-primary mx-1">btn-primary</button>
-        <button className="btn btn-danger mx-1">btn-danger</button>
+    <div className="App container pt-4">
+      <h1 className="text-center mb-4">Boilerplate</h1>
+      <div className="col-6 text-center">
+        <div className="form-row mt-3">
+          <input className="form-control col" type="text" />
+          <button className="btn btn-custom-color ml-2">Custom button</button>
+        </div>
+      </div>
     </div>
   );
 }
