cddceb12d89b93d2a5db53fecb89d86cd1b88aa4
cbertinci <jcbp.jcbp@gmail.com>
2020-08-31 22:15:37 -0300

    feat: add carousel to landing page

--- src/pages/Landing/Carousel.js
+++ src/pages/Landing/Carousel.js
@@ -2,28 +2,59 @@ import React, { Component } from 'react';
 import './landing.scss';
 
 class Carousel extends Component {
+  state = {
+    items: [
+      {text: 'A', css: 'bg-secondary'},
+      {text: 'B', css: 'bg-info'},
+      {text: 'C', css: 'bg-danger'}
+    ],
+    itemActive: 0
+  };
+  nextItem() {
+    const itemActive = this.state.itemActive;
+    this.setState({
+      itemActive: (itemActive + 1) % this.state.items.length
+    });
+  }
+  prevItem() {
+    const itemActive = this.state.itemActive;
+    this.setState({
+      itemActive: itemActive > 0
+        ? itemActive - 1
+        : this.state.items.length - 1
+    });
+  }
 	render() {
 		return (
       <div className="carousel slide" data-ride="carousel">
         <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="d-block bg-gray-300">a</div>
-          </div>
-          <div className="carousel-item">
-            <div className="d-block bg-gray-300">b</div>
-          </div>
-          <div className="carousel-item">
-            <div className="d-block bg-gray-300">c</div>
-          </div>
+          {this.state.items.map((item, index) => {
+            return <div className={
+                'carousel-item text-center' +
+                (index === this.state.itemActive
+                  ? ' active'
+                  : ''
+                )
+              }
+              key={index}
+            >
+              <h1 className={
+                'container carousel-item-content xy-center bg-gray-300 ml-auto mr-auto' +
+                ' ' + item.css
+              }>
+                {item.text}
+              </h1>
+            </div>
+          })}
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <div className="carousel-control-prev cursor-pointer" onClick={() => this.prevItem()}>
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        </div>
+        <div className="carousel-control-next cursor-pointer" onClick={() => this.nextItem()}>
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
-        </a>
+        </div>
       </div>
 		);
 	}
